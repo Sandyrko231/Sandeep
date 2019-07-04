97dcc4842c76b18995de9b8b4ba3e9f8bc26c521
Sandyrko231 <52520397+Sandyrko231@users.noreply.github.com>
2019-07-04 16:15:19 +0530

    Update Sandeep.java

--- src/Sandeep.java
+++ src/Sandeep.java
@@ -1,13 +1,1 @@
 
-public class Sandeep {
-public static void main(String [] args) {
-	System.out.println("Sandeep");
-	System.out.println("Lets do this");
-	System.out.println("Yooooo");
-	int a,b;
-	int c=a+b;
-	System.out.println(c);
-	int d;
-	
-}
-}
